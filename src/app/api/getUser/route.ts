2dbe839ad382a37a39002efe0443eae2ae1734de
RayanDri <rayan.drissi@icloud.com>
2025-03-23 06:44:00 +0100

    add @types/base-64 to devDependencies and refactor getUser route for improved readability

--- src/app/api/getUser/route.ts
+++ src/app/api/getUser/route.ts
@@ -1,50 +1,58 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { Buffer } from 'buffer'
-import base64 from 'base-64'
-import {QubicHelper} from "@qubic-lib/qubic-ts-library/dist/qubicHelper";
-
-export const HEADERS = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
-}
-
-export const makeJsonData = (contractIndex: any, inputType: any, inputSize: any, requestData: any) => {
-    return {
-        contractIndex: contractIndex,
-        inputType: inputType,
-        inputSize: inputSize,
-        requestData: requestData,
-    }
-}
-
-export const HM25_CONTRACT_INDEX = 12
-export const FUNCTION_ID = 1
+import { NextRequest, NextResponse } from "next/server";
+import { Buffer } from "buffer";
+import base64 from "base-64";
+import { QubicHelper } from "@qubic-lib/qubic-ts-library/dist/qubicHelper";
 
 export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams
-    const ID = searchParams.get('ID')
+  const HEADERS = {
+    accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
-    const helper = new QubicHelper()
-    const identityBytes = helper.getIdentityBytes(ID!)
+  const makeJsonData = (
+    contractIndex: any,
+    inputType: any,
+    inputSize: any,
+    requestData: any
+  ) => {
+    return {
+      contractIndex: contractIndex,
+      inputType: inputType,
+      inputSize: inputSize,
+      requestData: requestData,
+    };
+  };
 
-    const base64Encoded = Buffer.from(identityBytes).toString('base64')
-    const queryData = makeJsonData(HM25_CONTRACT_INDEX, FUNCTION_ID, 32, base64Encoded)
-    const response = await fetch(`http://46.17.103.110/v1/querySmartContract`, {
-        method: 'POST',
-        headers: HEADERS,
-        body: JSON.stringify(queryData),
-    })
-    if (!response.ok) {
-        return NextResponse.error()
-    }
-    const json = await response.json()
-    if (!json.responseData) {
-        return NextResponse.error()
-    }
+  const HM25_CONTRACT_INDEX = 12;
+  const FUNCTION_ID = 1;
+  const searchParams = request.nextUrl.searchParams;
+  const ID = searchParams.get("ID");
 
-    const raw = base64.decode(json.responseData)
-    const buf = Buffer.from(raw, 'binary')
+  const helper = new QubicHelper();
+  const identityBytes = helper.getIdentityBytes(ID!);
 
-    return NextResponse.json({ balance: buf.readBigUInt64LE(0).toString() });
+  const base64Encoded = Buffer.from(identityBytes).toString("base64");
+  const queryData = makeJsonData(
+    HM25_CONTRACT_INDEX,
+    FUNCTION_ID,
+    32,
+    base64Encoded
+  );
+  const response = await fetch(`http://46.17.103.110/v1/querySmartContract`, {
+    method: "POST",
+    headers: HEADERS,
+    body: JSON.stringify(queryData),
+  });
+  if (!response.ok) {
+    return NextResponse.error();
+  }
+  const json = await response.json();
+  if (!json.responseData) {
+    return NextResponse.error();
+  }
+
+  const raw = base64.decode(json.responseData);
+  const buf = Buffer.from(raw, "binary");
+
+  return NextResponse.json({ balance: buf.readBigUInt64LE(0).toString() });
 }
-
