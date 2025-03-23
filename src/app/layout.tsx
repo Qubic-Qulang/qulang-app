d4544501290a20ddb61a8aaae27ebb1d49deba02
RayanDri <rayan.drissi@icloud.com>
2025-03-23 10:49:35 +0100

    Refactor layout component for improved readability and consistent formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import {ConfigProvider} from "@/contexts/ConfigContext";
-import {QubicConnectCombinedProvider} from "@/contexts/QubicConnectContext";
-import {HM25Provider} from "@/contexts/QuLangContext";
+import { ConfigProvider } from "@/contexts/ConfigContext";
+import { QubicConnectCombinedProvider } from "@/contexts/QubicConnectContext";
+import { HM25Provider } from "@/contexts/QuLangContext";
 
 const spaceGroteskSans = Space_Grotesk({
   variable: "--font-space-grotesk-sans",
@@ -25,19 +25,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${spaceGroteskSans.variable} antialiased`}>
-      <ConfigProvider>
+        <ConfigProvider>
           <QubicConnectCombinedProvider>
-              <HM25Provider>
-                  <div className="flex flex-col min-h-dvh ">
-                      <Header />
-                      <div className="flex-1 flex flex-col">
-                          {children}
-                      </div>
-                      <Footer />
-                  </div>
-              </HM25Provider>
+            <HM25Provider>
+              <div className="flex flex-col min-h-dvh ">
+                <Header />
+                <div className="flex-1 flex flex-col">{children}</div>
+                <Footer />
+              </div>
+            </HM25Provider>
           </QubicConnectCombinedProvider>
-      </ConfigProvider>
+        </ConfigProvider>
       </body>
     </html>
   );
