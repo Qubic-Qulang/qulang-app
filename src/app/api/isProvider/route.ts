b897ae93071445f6b9d7a73f4ad516c4f303e15a
RayanDri <rayan.drissi@icloud.com>
2025-03-23 08:56:45 +0100

    isProvider

--- src/app/api/isProvider/route.ts
+++ src/app/api/isProvider/route.ts
@@ -1,49 +1,34 @@
-import { useQubicConnect } from '@/contexts/QubicConnectContext';
-import { useQuLang } from '@/contexts/QuLangContext';
-import { Pool } from 'pg';
-
+import { NextRequest, NextResponse } from "next/server";
+import { Pool } from "pg";
 
 const pool = new Pool({
   host: process.env.DB_HOST || "46.17.103.110",
   database: process.env.DB_NAME || "postgres",
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "sabirestgay",
-  ssl: false
+  ssl: false,
 });
 
+export async function GET(request: NextRequest) {
+  const HEADERS = {
+    accept: "application/json",
+    "Content-Type": "application/json",
+  };
 
-export async function isProvider() {
-  const { connected, showConnectModal, toggleConnectModal } = useQubicConnect();
-  const { balance, fetchBalance, walletPublicIdentity } = useQuLang();
-  
-  if (!connected || !walletPublicIdentity) {
-    return {
-      status: 400,
-      body: false
-    }
-  }
   const client = await pool.connect();
   try {
-    const res = await client.query('SELECT * FROM provider WHERE identity = $1', [walletPublicIdentity]);
+    const res = await client.query(
+      "SELECT * FROM provider WHERE identity = $1",
+      [request.nextUrl.searchParams.get("walletPublicIdentity")]
+    );
 
     if (res.rows.length === 0) {
-      return {
-        status: 404,
-        body: false
-      }
+      return NextResponse.json({ isProvider: false });
+    } else {
+      return NextResponse.json({ isProvider: true });
     }
-      else
-      {
-        return {
-          status: 200,
-          body: true
-        }
-      }
-  } catch (err : any) {
-    return {
-      status: 500,
-      body: err.message
-    }
+  } catch (err: any) {
+    return NextResponse.error();
   } finally {
     client.release();
   }
