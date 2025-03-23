35b7dfa46c7b44ae8930c5e3652fa30be69813e2
RayanDri <rayan.drissi@icloud.com>
2025-03-23 10:51:06 +0100

    Refactor layout component for improved code consistency and readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,38 @@
-import type { Metadata } from "next";
-import { Space_Grotesk } from "next/font/google";
-import "./globals.css";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import { ConfigProvider } from "@/contexts/ConfigContext";
-import { QubicConnectCombinedProvider } from "@/contexts/QubicConnectContext";
-import { HM25Provider } from "@/contexts/QuLangContext";
+import type { Metadata } from "next"
+import { Space_Grotesk } from "next/font/google"
+import "./globals.css"
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
+import { ConfigProvider } from "@/contexts/ConfigContext"
+import { QubicConnectCombinedProvider } from "@/contexts/QubicConnectContext"
+import { HM25Provider } from "@/contexts/QuLangContext"
 
 const spaceGroteskSans = Space_Grotesk({
   variable: "--font-space-grotesk-sans",
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "QuLang app",
   description: "Welcome to the QuLang dApp !",
-};
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={`${spaceGroteskSans.variable} antialiased`}>
         <ConfigProvider>
           <QubicConnectCombinedProvider>
             <HM25Provider>
-              <div className="flex flex-col min-h-dvh ">
+              <div className="flex flex-col min-h-dvh">
                 <Header />
-                <div className="flex-1 flex flex-col">{children}</div>
+                <div className="flex-1 flex flex-col">
+                  {children}
+                </div>
                 <Footer />
               </div>
             </HM25Provider>
@@ -38,5 +40,5 @@ export default function RootLayout({
         </ConfigProvider>
       </body>
     </html>
-  );
+  )
 }
